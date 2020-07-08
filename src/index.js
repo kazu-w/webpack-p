14c0cc595bc8dd5bb0f201618c3b8b90cd88e7a4
kw <62018635+kazu-w@users.noreply.github.com>
2020-07-08 22:19:32 +0900

    w1

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import "./modules/style.css";
+
+const dogs = {
+  shiba: {
+    name: "shiba",
+    nation: "ja",
+  },
+  akita: {
+    name: "akita",
+    nation: "ja",
+  },
+  tosa: {
+    name: "tosa",
+    nation: "ja",
+  },
+  husky: {
+    name: "husky",
+    nation: "ro",
+  },
+};
+console.log(dogs);
+
+const filterdDogs = (nation) => {
+  //return; //Object.fromEntries(
+  return Object.entries(dogs).filter(([, dog]) => {
+    return dog.nation === nation;
+  });
+  //);
+};
+
+const searchBtn = document.getElementById("search");
+const selectNation = document.getElementById("selectNation");
+const result = document.getElementById("result");
+let nation;
+let getData;
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  nation = selectNation.value;
+  getData = filterdDogs(nation);
+  result.innerHTML = "";
+  getData.forEach(([, data]) => {
+    const p = document.createElement("p");
+    p.innerHTML = `${data.name} ${data.nation}`;
+    result.appendChild(p);
+  });
+});
+
+import m1 from "./modules/m1";
+
+m1();
